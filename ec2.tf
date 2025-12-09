b6ba7196fa358eb831eecd7c2575a0461f12def6
bipendevmohanboda <bipendev@gmail.com>
2025-12-08 21:25:43 -0500

    terraform folder added

--- ec2.tf
+++ ec2.tf
@@ -4,13 +4,23 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = var.sg_ids # mandatory check in variables
   tags = var.tags # optional - check in variables
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo dnf -y install dnf-plugins-core
-              sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-              sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              sudo usermod -aG docker ec2-user
-              EOF
+  # user_data = <<-EOF
+  # #!/bin/bash
+  # sudo dnf -y install dnf-plugins-core
+  # sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+  # sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  # sudo systemctl start docker
+  # sudo systemctl enable docker
+  # sudo usermod -aG docker ec2-user
+  # EOF
+
+user_data = <<-EOF
+#!/bin/bash
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+EOF
 }
